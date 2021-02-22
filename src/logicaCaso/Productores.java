3dbb1ac88b84af78c4a1ef7a4035deb0fcd94463
Alejandro A <a.ahogado@uniandes.edu.co>
2021-02-22 15:54:11 -0500

    Metodos iniciales Productor

--- src/logicaCaso/Productores.java
+++ src/logicaCaso/Productores.java
@@ -5,8 +5,39 @@ public class Productores extends Thread {
 	@Override
 	public void run()
 	{
-		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Productor " + i);
+
+			try {
+				Thread.sleep((int)Math.random()*1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	
+
+	public int numeroProductores = 10;
+	public int numeroProductosA = 0;
+	public int numeroProductosB = 0;
+	public final static int MAX_PRODUCTOS = 10;
+	public boolean generando = false;
+
+	private synchronized void generarProductoA() {
+		
+		if (numeroProductosA+numeroProductosB<=MAX_PRODUCTOS ) {
+			generando = true;
+			numeroProductosA++;
+		}
+		generando = false;
+	}
+
+	private synchronized void generarProductoB() {
+			if (numeroProductosA+numeroProductosB<=MAX_PRODUCTOS ) {
+				generando = true;
+				numeroProductosB++;
+			}
+			generando = false;
+	}
+
+
 }
